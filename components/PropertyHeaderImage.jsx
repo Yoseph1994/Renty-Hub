0e17c66e0c618643a232415c01c68dfdb39ca025
Yoseph1994 <joshrde2002@example.com>
2024-07-16 16:40:43 +0300

    added spinner for client side fetching, made propertyDetails component dynamic from the tempalte files

--- components/PropertyHeaderImage.jsx
+++ components/PropertyHeaderImage.jsx
@@ -1,7 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
-function PropertyHeaderImage() {
-  return <div>PropertyHeaderImage</div>;
+function PropertyHeaderImage({ image }) {
+  return (
+    <section>
+      <div className="container-xl m-auto">
+        <div className="grid grid-cols-1">
+          <Image
+            src={`/images/properties/${image}`}
+            alt="Header Img"
+            className="object-cover h-[400px] w-full"
+            width={0}
+            sizes="100vw"
+            height={0}
+            priority={true}
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default PropertyHeaderImage;
---
+++ components/Spinner.jsx
@@ -0,0 +1,22 @@
+"use client";
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+function Spinner({ loading }) {
+  return (
+    <div>
+      <ClipLoader
+        color="#3b82f6"
+        loading={loading}
+        cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+      />
+    </div>
+  );
+}
+
+export default Spinner;
